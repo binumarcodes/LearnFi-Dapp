3ab6a284bfdb220427b6f34fa45fc1cbba5e08f5
binumarcodes <abdulrahmanbinumar179@gmail.com>
2025-08-11 22:52:01 +0100

    added use clinet to uploadcontent

--- apps/demo-app/next.config.js
+++ apps/demo-app/next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [], // add image domains here if needed
+  },
 };
+
+module.exports = nextConfig;
